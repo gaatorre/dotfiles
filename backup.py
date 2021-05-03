a2a6af5bc54b0044c84219262a36415d38a05e3a
Gabriel Torres <gaatorre@ucsc.edu>
2021-05-03 12:46:46 -0700

    Updating dotfiles

---
+++ backup.py
@@ -0,0 +1,19 @@
+import os
+
+dotfiles = ["~/.config/nvim"]
+
+def git_update():
+    os.system("git add .")
+    os.system("git commit -m \"Updating dotfiles\"")
+    os.system("git push")
+
+def main():
+    print("Backing up dotfiles")
+
+    for i in dotfiles:
+        os.system(f"cp -r {i} .")
+
+    git_update()
+
+if __name__ == "__main__":
+    main()
